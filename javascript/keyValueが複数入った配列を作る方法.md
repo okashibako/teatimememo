9a1bc9c371f0b809244a9ebef8c220b9174bb733
asaba <swift0910@cpoint-lab.co.jp>
2020-11-19 14:43:41 +0900

    ファイル削除

--- javascript/keyValueが複数入った配列を作る方法.md
+++
@@ -1,8 +0,0 @@
-var arr = [
-    { label: "静岡県", value: 22},
-    { label: "愛知県", value: 21},
-]
-
-//console.logで表すとこんな感じ
-0: {label: "静岡県", value: 22}
-1: {label: "愛知県", value: 21}
--- javascript/配列の中にオブジェクトを入れる方法.js
+++
@@ -1,7 +0,0 @@
-const infra = [
-    {id: "1", key:"hasWater", name: "水道", value: true},
-    {id: "2", key:"hasElectricity", name: "電気", value: true},
-    {id: "3", key:"hasGas", name: "ガス", value: true},
-    {id: "4", key:"hasFixedLinePhone", name: "固定電話", value: true},
-    {id: "5", key:"hasInternet", name: "インターネット", value: true},
-  ];
